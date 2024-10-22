9cf6e7065966bcf9f6fd5626da529290066c8c85
Zolton21 <zolton21@users.noreply.github.com>
2024-10-22 20:05:08 +0200

    Fixed control offset

---
+++ src/main/java/net/mistersecret312/temporal_api/events/CommonEvents.java
@@ -0,0 +1,58 @@
+package net.mistersecret312.temporal_api.events;
+
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.mistersecret312.temporal_api.TemporalAPIMod;
+import net.tardis.mod.helper.TardisHelper;
+import net.tardis.mod.helper.WorldHelper;
+import net.tardis.mod.tileentities.ConsoleTile;
+import net.tardis.mod.world.dimensions.TDimensions;
+
+import java.util.*;
+
+@Mod.EventBusSubscriber(modid = TemporalAPIMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class CommonEvents {
+
+    @SubscribeEvent
+    public static void onServerStarting(FMLServerStartingEvent event){
+
+        event.getServer().getWorlds();
+        List<RegistryKey<World>> worlds = new ArrayList<>(Collections.emptyList());
+        for(ServerWorld world : event.getServer().getWorlds()){
+            if(WorldHelper.areDimensionTypesSame(world, TDimensions.DimensionTypes.TARDIS_TYPE)) {
+                worlds.add(world.getDimensionKey());
+            }
+        }
+
+        if(!worlds.isEmpty()) {
+            int size = worlds.size();
+            for(int i = 0; i < size; i++) {
+                if (TardisHelper.getConsoleInWorld(event.getServer().getWorld(worlds.get(i))).isPresent()) {
+                    Optional<ConsoleTile> console = TardisHelper.getConsoleInWorld(event.getServer().getWorld(worlds.get(i)));
+                    console.ifPresent((tile -> {
+                        System.out.println("recreating controls");
+                        tile.removeControls();
+                        tile.getOrCreateControls();
+                    }));
+                }
+            }
+        }
+    }
+    /*
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        if(event.getWorld().getDimensionType().equals(TDimensions.TARDIS_DIMENSIONS)) {
+            if(TardisHelper.getConsoleInWorld(((ServerWorld) event.getWorld()).getWorld()).isPresent()){
+                Optional<ConsoleTile> console = TardisHelper.getConsoleInWorld(((ServerWorld) event.getWorld()).getWorld());
+                console.ifPresent((tile -> {
+                    tile.removeControls();
+                    tile.getOrCreateControls();
+                }));
+            }
+        }
+    }*/
+}
